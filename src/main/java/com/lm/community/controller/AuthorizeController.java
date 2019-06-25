373986866e663f1e86f0284f584da154203b48f9
zhenglimei <1808123642@qq.com>
2019-06-25 16:00:08 +0800

    extract github config from code to properties

---
+++ src/main/java/com/lm/community/controller/AuthorizeController.java
@@ -0,0 +1,37 @@
+package com.lm.community.controller;
+
+import com.lm.community.dto.AccessTokenDTO;
+import com.lm.community.provide.GitHubProvide;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthorizeController {
+    @Autowired
+    private GitHubProvide gitHubProvide;
+
+    @Value("${github.client_id}")
+    private String clientId;
+    @Value("${github.client_secret}")
+    private String clientSecret;
+    @Value("${github.redirect_uri}")
+    private String redirectUri;
+
+
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri(redirectUri);
+        accessTokenDTO.setState(state);
+        gitHubProvide.getAccessToken(accessTokenDTO);
+        return "index";
+    }
+}
